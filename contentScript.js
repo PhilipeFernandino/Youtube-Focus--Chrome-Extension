2324172cc488ea6d8e429fb9998df55264d776b0
PhilipeFernandino <57276004+PhilipeFernandino@users.noreply.github.com>
2021-06-27 13:39:16 -0300

    Começando e finalizando extensão

---
+++ contentScript.js
@@ -0,0 +1,29 @@
+const delElements = ['ytd-rich-grid-renderer', '#secondary', '.ytd-comments',' [aria-label="Explorar"]', '[title="Explorar"]'];
+const mutationObserver = new MutationObserver(onMutation);
+onMutation([{addedNodes: [document.documentElement]}]);
+observe();
+
+function onMutation(mutations) {
+    const toRemove = [];
+    for (const { addedNodes } of mutations) {
+        for (const n of addedNodes) {
+            if (n.tagName) {
+                if (n.matches(delElements)) {
+                    toRemove.push(n);
+                } else if (n.firstElementChild && n.querySelector(delElements)) {
+                    toRemove.push(...n.querySelectorAll(delElements));
+                }
+            }
+        }
+    }
+    for (const element of toRemove) 
+        element.remove();
+        observe();
+}
+
+function observe() {
+    mutationObserver.observe(document, {
+        subtree: true,
+        childList: true,
+    });
+}
